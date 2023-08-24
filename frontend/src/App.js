d70fb7a740e839af6edc2e23835a3752f119d20c
Ruth Goeller <ruthgoeller@Ruths-Air.lan>
2023-08-24 12:23:31 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
 
 function App() {
   return (
@@ -9,11 +9,10 @@ function App() {
     <Header />
     <main className='py-3'>
     <Container>
-      <h1>Welcome to ProShop</h1>
+<Outlet/>
     </Container>
     </main>
     <Footer />
-
     </>
   )
 }
